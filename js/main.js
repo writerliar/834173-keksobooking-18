8fdbcae8fc17e54c56cdaeddde3725f2d2945a33
Dana <kovach.dana@yandex.ru>
2019-09-22 18:06:29 +0300

    создание предложений

--- js/main.js
+++ js/main.js
@@ -1,140 +1,102 @@
 'use strict';
 
-//здесь константы
 var MAX_ADVERTS = 8;
 var TYPES = ['palace', 'flat', 'house', 'bungalo'];
 var CHECKIN_TIME = ['12:00', '13:00', '14:00'];
 var CHECKOUT_TIME = ['12:00', '13:00', '14:00'];
 var FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
-var LIMIT = 1200;
+var LIMIT_X = 1200;
+var MIN_Y = 130;
+var MAX_Y = 630;
 
-//моки
 var titles = ['Отличная квартира с видом на вельзевула', 'Дом в самом сердце жерла вулкана', 'Хостел. Если боитесь крыс, берите с собой кошек', 'Матрас на заднем дворе. За доп плату дам зонт', 'Отличная квартира с видом на вельзевула', 'Дом в самом сердце жерла вулкана', 'Хостел. Если боитесь крыс, берите с собой кошек', 'Матрас на заднем дворе. За доп плату дам зонт'];
-var address = '600, 300';
+var address = '600, 350';
 var price = 2000;
 var rooms = 4;
 var guests = 5;
 var description = 'заселяйся - и не выселишься';
 var photos = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
 
-//получение рандомного элемента
+var map = document.querySelector('.map');
+var pinAdvertTemplate = document.querySelector('#pin').content.querySelector('.map__pin');
+var pinAdvertList = document.querySelector('.map__pins');
+
 var getRandomElement = function (array) {
   return array[Math.floor(Math.random() * array.length)];
 };
 
-var avatarImg = 'img/avatars/user01.png';
-
-
-
-//найти и разблокировать карту
-var map = document.querySelector('.map');
-map.classList.remove('map--faded');
-
-// найти шаблон пина, который будем копировать
-var pinAdvertTemplate = document.querySelector('#pin').content.querySelector('.map__pin');
-
-// найти элемент, в который будем добавлять пины
-var pinAdvertList = document.querySelector('.map__pins');
-
-// создать одно предложение
-var makeAdvert = function () {
-  for (var j = 1; j < MAX_ADVERTS; j++) {
-    avatarImg = 'img/avatars/user0' + j + '.png';
-  }
-  var similarAdvert = [
-    {
-      author: {
-        avatar: avatarImg,
-      },
-      offer: {
-        title: 'отличная квартира с видом на вельзевула',
-        address: '600, 350',
-        price: 5000,
-        type: 'bungalo',
-        rooms: 3,
-        guests: 5,
-        checkin: '12:00',
-        checkout: '14:00',
-        features: 'wifi',
-        description: 'descripyion',
-        photos: ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg']
-      },
-      location: {
-        x: 500 + 'px', //случайное число, координата x метки на карте. Значение ограничено размерами блока, в котором перетаскивается метка
-        y: 130 + 'px' //случайное число, координата y метки на карте от 130 до 630
-      }
-    },
-    {
-      author: {
-        avatar: avatarImg,
-      },
-      offer: {
-        title: 'отличная квартира с видом на вельзевула',
-        address: '600, 350',
-        price: 5000,
-        type: 'bungalo',
-        rooms: 3,
-        guests: 5,
-        checkin: '12:00',
-        checkout: '14:00',
-        features: 'wifi',
-        description: 'descripyion',
-        photos: ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg']
-      },
-      location: {
-        x: 700 + 'px', //случайное число, координата x метки на карте. Значение ограничено размерами блока, в котором перетаскивается метка
-        y: 630 + 'px' //случайное число, координата y метки на карте от 130 до 630
-      }
-    },
-    {
-      author: {
-        avatar: avatarImg,
-      },
-      offer: {
-        title: 'отличная квартира с видом на вельзевула',
-        address: '600, 350',
-        price: 5000,
-        type: 'bungalo',
-        rooms: 3,
-        guests: 5,
-        checkin: '12:00',
-        checkout: '14:00',
-        features: 'wifi',
-        description: 'descripyion',
-        photos: ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg']
-      },
-      location: {
-        x: 400 + 'px', //случайное число, координата x метки на карте. Значение ограничено размерами блока, в котором перетаскивается метка
-        y: 300 + 'px' //случайное число, координата y метки на карте от 130 до 630
-      }
-    }
-  ];
+var randomFromInterval = function (min, max) {
+  var number = min + Math.random() * (max + 1 - min);
+  return Math.floor(number);
 };
 
-// добавим 8 шаблонов предложений
-for (var i = 0; i < MAX_ADVERTS; i++) {
+var randomArray = function (array) {
+  var newArray = [];
+  var newLength = randomFromInterval(0, array.length);
+
+  for (var i = 0; i < newLength; i++) {
+    newArray.push(array[i]);
+  }
+
+  return newArray;
+};
+
+var createAdvert = function (id) {
+  return {
+    author: {
+      avatar: 'img/avatars/user0' + id + '.png',
+    },
+    offer: {
+      title: getRandomElement(titles),
+      address: address,
+      price: price,
+      type: getRandomElement(TYPES),
+      rooms: rooms,
+      guests: guests,
+      checkin: getRandomElement(CHECKIN_TIME),
+      checkout: getRandomElement(CHECKOUT_TIME),
+      features: randomArray(FEATURES),
+      description: description,
+      photos: randomArray(photos)
+    },
+    location: {
+      x: randomFromInterval(0, LIMIT_X) + 'px',
+      y: randomFromInterval(MIN_Y, MAX_Y) + 'px'
+    }
+  }
+};
+
+var generateAdverts = function (num) {
+  var adverts = [];
+
+  for (var i = 1; i < num; i++) {
+    adverts.push(createAdvert(i));
+  }
+
+  return adverts;
+};
+
+var renderAdvert = function (advert) {
   var advertItem = pinAdvertTemplate.cloneNode(true);
 
-  advertItem.querySelector('img').src = similarAdvert[i].author.avatar;
-  advertItem.querySelector('img').alt = similarAdvert[i].offer.title;
-  advertItem.style.left = similarAdvert[i].location.x;
-  advertItem.style.top = similarAdvert[i].location.y;
+    advertItem.querySelector('img').src = advert.author.avatar;
+    advertItem.querySelector('img').alt = advert.offer.title;
+    advertItem.style.left = advert.location.x;
+    advertItem.style.top = advert.location.y;
 
-  pinAdvertList.appendChild(advertItem);
-}
+    return advertItem;
+};
 
-//создаем массив из предложений
+var addAdvert = function (adverts) {
+  var fragment = document.createDocumentFragment();
 
-//автоматизируем его заполнение
+  adverts.forEach(function (advert) {
+    fragment.appendChild(renderAdvert(advert));
+  });
 
+  pinAdvertList.appendChild(fragment);
+};
 
-// создадим массив на основе предложений
+map.classList.remove('map--faded');
 
-
-
-
-//Координаты:style="left: {{location.x}}px; top: {{location.y}}px;"
-// src="{{author.avatar}}"
-// alt="{{заголовок объявления}}"
-
-
+addAdvert(generateAdverts(MAX_ADVERTS));
