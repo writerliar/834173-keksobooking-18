b5614473593741166482f7bed1b9bebe39cac561
Dana Kovach <kovach.dana@yandex.ru>
2019-11-06 17:59:26 +0300

    фильтр по цене

--- js/filter.js
+++ js/filter.js
@@ -5,7 +5,23 @@
 
   var filterAdverts = document.querySelector('.map__filters');
   var typeSelector = filterAdverts.querySelector('#housing-type');
+  var priceSelector = filterAdverts.querySelector('#housing-price');
+  var roomsSelector = filterAdverts.querySelector('#housing-rooms');
   var filterFormList = document.querySelectorAll('.map__filter, .map__checkbox');
+
+  var convertLevelToPrice = function (price) {
+    if (price > 50000) {
+      return 'high';
+    }
+
+    if (price < 10000) {
+      return 'low';
+    }
+
+    if (price >= 10000 || price <= 50000) {
+      return 'middle';
+    }
+  };
 
   var activateFilter = function () {
     filterFormList.forEach(window.util.unsetDisabled);
@@ -20,8 +36,20 @@
       || typeSelector.value === advert.offer.type;
   };
 
+  var filterPrice = function (advert) {
+    return priceSelector.value === 'any'
+      || priceSelector.value === convertLevelToPrice(advert.offer.price);
+  };
+
+  // var filterRooms = function (advert) {
+  //   return roomsSelector.value === 'any'
+  //   ||  roomsSelector.value === advert.offer.rooms;
+  // };
+
   var filterAdvert = function (advert) {
-    return filterType(advert);
+    return filterType(advert)
+      && filterPrice(advert);
+      // && filterRooms(advert);
   };
 
   var updateFilter = function () {
@@ -31,6 +59,8 @@
 
     window.pin.delete();
     window.pin.add(filteredAdverts);
+
+    console.log(window.pin.adverts.filter(filterAdvert));
   };
 
   var onFilterChange = function () {
