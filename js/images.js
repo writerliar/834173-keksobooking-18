62a8766f426851d79dff7b7e8483a80fa693dfc3
Dana <kovach.dana@yandex.ru>
2019-11-07 20:22:58 +0300

    загрузка картинок

--- js/images.js
+++ js/images.js
@@ -6,36 +6,12 @@
   var avatarFileChooser = document.querySelector('#avatar');
   var avatarPreview = document.querySelector('.ad-form-header__preview img');
 
-  var advertFileChooser = document.querySelector('#images');
+  var advertPhotoChooser = document.querySelector('#images');
   var advertPhotoPreview = document.querySelector('.ad-form__photo');
 
   var onImageLoad = function (evt) {
-    console.log(evt.target);
-  };
-
-  avatarFileChooser.addEventListener('change', onImageLoad);
-
-  // avatarFileChooser.addEventListener('change', function () {
-  //   var file = avatarFileChooser.files[0];
-  //   var fileName = file.name.toLowerCase();
-  //
-  //   var matches = FILE_TYPES.some(function (it) {
-  //     return fileName.endsWith(it);
-  //   });
-  //
-  //   if (matches) {
-  //     var reader = new FileReader();
-  //
-  //     reader.addEventListener('load', function () {
-  //       avatarPreview.src = reader.result;
-  //     });
-  //
-  //     reader.readAsDataURL(file);
-  //   }
-  // });
-
-  advertFileChooser.addEventListener('change', function () {
-    var file = advertFileChooser.files[0];
+    var fileChooser = evt.target;
+    var file = fileChooser.files[0];
     var fileName = file.name.toLowerCase();
 
     var matches = FILE_TYPES.some(function (it) {
@@ -46,12 +22,21 @@
       var reader = new FileReader();
 
       reader.addEventListener('load', function () {
-        advertPhotoPreview.style.backgroundImage = 'url(' + reader.result + ')';
-        advertPhotoPreview.style.backgroundSize = 'cover';
+        if (fileChooser === avatarFileChooser) {
+          avatarPreview.src = reader.result;
+        }
+
+        if (fileChooser === advertPhotoChooser) {
+          advertPhotoPreview.style.backgroundImage = 'url(' + reader.result + ')';
+          advertPhotoPreview.style.backgroundSize = 'cover';
+        }
       });
 
       reader.readAsDataURL(file);
     }
-  });
+  };
+
+  avatarFileChooser.addEventListener('change', onImageLoad);
+  advertPhotoChooser.addEventListener('change', onImageLoad);
 }
 )();
